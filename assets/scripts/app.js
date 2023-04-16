95e7f62d3063fb9cf66adbd34617febd0e9fd46b
TekniqueJoseph <joejacksun@gmail.com>
2023-04-16 17:54:14 -0400

    consolidated functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,7 +13,6 @@ function createAndWriteLog(resultBeforeCalc, operator, calcNum) {
     outputResult(currentResult, calcDesc); // from vendor file
 }
 
-// Gets and creates Object logEntry
 function writeToLog(opId, prevResult, opNum, newResult) {
     const logEntry = {
         operation: opId,
@@ -25,20 +24,27 @@ function writeToLog(opId, prevResult, opNum, newResult) {
     console.log(logEntries)
 }
 
-function add() {
+function calculateResult(calculationType) {
     const enteredNumber = getUserNumberInput() 
     const initialResult = currentResult;
-    currentResult += enteredNumber;
-    createAndWriteLog(initialResult, '+', enteredNumber)
-    writeToLog('ADD', initialResult, enteredNumber, currentResult)
+    let mathOperator;
+    if (calculationType === 'ADD') {
+        currentResult += enteredNumber;
+        mathOperator = '+'
+    } else {
+        currentResult -= enteredNumber;
+        mathOperator = '-'
+    }
+    createAndWriteLog(initialResult, mathOperator, enteredNumber)
+    writeToLog(calculationType, initialResult, enteredNumber, currentResult) 
+}
+
+function add() {
+    calculateResult('ADD')
 }
 
 function subtract() {
-    const enteredNumber = getUserNumberInput() 
-    const initialResult = currentResult;
-    currentResult -= enteredNumber;
-    createAndWriteLog(initialResult, '-', enteredNumber)
-    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult)
+    calculateResult('SUBTRACT')
 }
 
 function multiply() {
