2c426a0380bc814ed42b1171b1410b9b1a9897a5
TekniqueJoseph <joejacksun@gmail.com>
2023-04-17 22:48:26 -0400

    added logical operators

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -28,6 +28,24 @@ function writeToLog(opId, prevResult, opNum, newResult) {
 
 function calculateResult(calculationType) {
     const enteredNumber = getUserNumberInput() 
+    if(
+        calculationType !== 'ADD' &&
+        calculationType !== 'SUNTRACT' &&
+        calculationType !== 'MULTIPLY' &&
+        calculationType !== 'DIVIDE' ||
+        !enteredNumber
+    ) {
+        return;
+    }
+    // if(
+    //     calculationType === 'ADD' ||
+    //     calculationType === 'SUNTRACT' ||
+    //     calculationType === 'MULTIPLY' ||
+    //     calculationType === 'DIVIDE'
+    // ) {
+    //     return;
+    // }
+    
     const initialResult = currentResult;
     let mathOperator;
     if (calculationType === 'ADD') {
@@ -43,6 +61,8 @@ function calculateResult(calculationType) {
         currentResult /= enteredNumber;
         mathOperator = '/'
     }
+
+
     createAndWriteLog(initialResult, mathOperator, enteredNumber)
     writeToLog(calculationType, initialResult, enteredNumber, currentResult) 
 }
@@ -78,22 +98,30 @@ divideBtn.addEventListener('click', divide);
 // solve()
 // console.log(numbers)
 
-let userCategory; // should be 'child', 'adult' or 'senior'
-let age = 30;
+// let userCategory; // should be 'child', 'adult' or 'senior'
+// let age = 30;
 
-function solve() {
-    if (age < 18) {
-        userCategory = 'child'
-        return userCategory
-    } else if (age >= 18 & age < 65) {
-        userCategory = 'adult'
-        return userCategory
-    } else {
-        userCategory = 'senior'
-        return userCategory
-    }
-}
+// function solve() {
+//     if (age < 18) {
+//         userCategory = 'child'
+//         return userCategory
+//     } else if (age >= 18 & age < 65) {
+//         userCategory = 'adult'
+//         return userCategory
+//     } else {
+//         userCategory = 'senior'
+//         return userCategory
+//     }
+// }
 // console.log(solve())
 
 
 
+let age;
+if (age) {
+    console.log('a')
+} else if (age === null) {
+    console.log('b')
+} else {
+    console.log('c')
+}
